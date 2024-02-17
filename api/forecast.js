ae6ed3af959d43f4ac8eeb7faaa0ebb62f48e867
Polibeerex <polibeerex@gmail.com>
2024-02-17 23:42:16 +0100

    Catching errors

--- api/forecast.js
+++ api/forecast.js
@@ -7,12 +7,26 @@ export default async (req, res) => {
 
     const geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiOpenWeather}`;
     const geocodingResponse = await fetch(geocodingUrl);
+    if (!geocodingResponse.ok) {
+      console.error(
+        "Error fetching geocoding data:",
+        geocodingResponse.statusText
+      );
+      throw new Error("Error fetching geocoding data");
+    }
     const geocodingData = await geocodingResponse.json();
     const lat = geocodingData[0].lat;
     const lon = geocodingData[0].lon;
 
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiOpenWeather}&units=metric`;
     const forecastResponse = await fetch(forecastUrl);
+    if (!forecastResponse.ok) {
+      console.error(
+        "Error fetching forecast data:",
+        forecastResponse.statusText
+      );
+      throw new Error("Error fetching forecast data");
+    }
     const forecastData = await forecastResponse.json();
 
     res.json(forecastData);
