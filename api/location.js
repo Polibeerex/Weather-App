cf2d9b96ee87bbd618333f424e4c930bbea8b39f
Polibeerex <polibeerex@gmail.com>
2024-02-17 23:24:06 +0100

    location.js

--- api/location.js
+++ api/location.js
@@ -1,25 +1,31 @@
 // api/location.js
-
-import fetch from "node-fetch";
-
 export default async (req, res) => {
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const apiOpenCage = process.env.OPENCAGE_API_KEY;
-  const response = await fetch(
-    `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${apiOpenCage}&language=en`
-  );
-  const data = await response.json();
+  try {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    const apiOpenCage = process.env.OPENCAGE_API_KEY;
 
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // and the headers you need for your own requests
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
+    const fetch = (await import("node-fetch")).default;
 
-  res.json(data);
+    const response = await fetch(
+      `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${apiOpenCage}&language=en`
+    );
+    const data = await response.json();
+
+    // Set CORS headers
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    );
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
 };
