be2250841314ab63fa6d10fbc429c7408d10d952
Polibeerex <polibeerex@gmail.com>
2024-02-17 23:35:35 +0100

    fixing

--- api/forecast.js
+++ api/forecast.js
@@ -1,17 +1,23 @@
 // api/forecast.js
-import fetch from "node-fetch";
-
 export default async (req, res) => {
   try {
-    const location = req.query.location;
-    const encodedLocation = encodeURIComponent(location);
+    const city = req.query.city;
     const apiOpenWeather = process.env.OPENWEATHERMAP_API_KEY;
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodedLocation}&appid=${apiOpenWeather}&units=metric`;
-    const weatherResponse = await fetch(weatherUrl);
-    const weatherData = await weatherResponse.json();
-    res.json(weatherData);
+    const fetch = (await import("node-fetch")).default;
+
+    const geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiOpenWeather}`;
+    const geocodingResponse = await fetch(geocodingUrl);
+    const geocodingData = await geocodingResponse.json();
+    const lat = geocodingData[0].lat;
+    const lon = geocodingData[0].lon;
+
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiOpenWeather}&units=metric`;
+    const forecastResponse = await fetch(forecastUrl);
+    const forecastData = await forecastResponse.json();
+
+    res.json(forecastData);
   } catch (error) {
-    console.error("Error in weather.js:", error);
+    console.error(error);
     res
       .status(500)
       .json({ error: "An error occurred while processing your request." });
--- api/weather.js
+++ api/weather.js
@@ -1,18 +1,18 @@
 // api/weather.js
-
-const fetch = require("node-fetch");
-
-module.exports = async (req, res) => {
+export default async (req, res) => {
   try {
     const location = req.query.location;
     const encodedLocation = encodeURIComponent(location);
     const apiOpenWeather = process.env.OPENWEATHERMAP_API_KEY;
+    const fetch = (await import("node-fetch")).default;
+
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodedLocation}&appid=${apiOpenWeather}&units=metric`;
     const weatherResponse = await fetch(weatherUrl);
     const weatherData = await weatherResponse.json();
+
     res.json(weatherData);
   } catch (error) {
-    console.error("Error in weather.js:", error);
+    console.error(error);
     res
       .status(500)
       .json({ error: "An error occurred while processing your request." });
